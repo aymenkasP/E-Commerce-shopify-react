5fd8103daeca3a84876477097c815f26af92edc5
aymenkasP <ikoaymen056@gmail.com>
2021-10-31 21:32:42 +0100

    fix api

--- src/Context/ShopifyContext.js
+++ src/Context/ShopifyContext.js
@@ -4,8 +4,8 @@ import Client from 'shopify-buy';
 
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
-  domain:process.env.REACT_SHOPIFY_DOMAIN,
-  storefrontAccessToken:process.env.REACT_SHOPIFY_TOKEN
+  domain:process.env.REACT_APP_SHOPIFY_DOMAIN,
+  storefrontAccessToken:process.env.REACT_APP_SHOPIFY_TOKEN
 });
 
 
